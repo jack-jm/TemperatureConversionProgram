95e5658e3cda4f18be8f13c5ba0cee81cc473e4d
jack-jm <101677953+jack-jm@users.noreply.github.com>
2023-08-21 00:58:09 +0000

    Started  with the first videos

---
+++ 01_convertor_gui_v1.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+class Converter:
+  print("hello world")
+
+# Main routine
+
+if __name__ == "__main__":
+  root = tK()
+  root.tittle("Temperature Converter")
+  Converter()
+  root.mainloop()
